a94da316adb9623b6d00b54f781b991f00cbcb13
TuzaGordie <gordie2u@gmail.com>
2025-05-23 08:01:18 +0100

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [CommonModule, FormsModule]
 })
 export class HomeComponent {
 
+  todos: any[] = [];
+  todo: string = ''
+
+  ngOnInit() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    this.todos = [
+      { item: 'Setup Project (Angular development)', completed: false },
+      { item: 'Create and setup Netlify Account', completed: false },
+      { item: 'Convert the UI/UX designs from Figma into HTML', completed: false },
+      { item: 'Apply CSS styles to Web pages after design conversion', completed: false },
+      { item: 'Host and deploy Converted designs or web pages on Netlify', completed: false },
+    ]
+  }
+
+  addItem() {
+    let todo = {
+      item: this.todo,
+      completed: false
+    }
+
+    this.todos.push(todo)
+    this.todo = ''
+
+  }
 }
